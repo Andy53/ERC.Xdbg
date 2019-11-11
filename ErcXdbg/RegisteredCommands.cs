d6068ab13b46ac76181557f34e51038c271d5811
Andy <35195301+Andy53@users.noreply.github.com>
2019-11-11 11:04:55 -0800

    Update RegisteredCommands.cs

--- ErcXdbg/RegisteredCommands.cs
+++ ErcXdbg/RegisteredCommands.cs
@@ -7,50 +7,52 @@ namespace ErcXdbg
 {
     public static class RegisteredCommands
     {
-        enum Command
-        {
-            Config,
-            Pattern,
-            Bytearray,
-            Compare,
-            Assemble,
-            Disassemble,
-            ListProcesses,
-            ProcessInfo,
-            ModuleInfo,
-            ThreadInfo,
-            SEH,
-            EggHunters,
-            FindNrp,
-            Rop 
-        }
-
         public static bool ErcCommand(int argc, string[] argv)
         {
-            var hProcess = Bridge.DbgValFromString("$hProcess");
-            if(argc <= 1)
+            try
             {
-                PrintHelp();
+                //Get the handle of the attached process
+                var hProcess = Bridge.DbgValFromString("$hProcess");
+
+                //Confirm that at least some options were passed.
+                if (argc <= 1)
+                {
+                    PrintHelp();
+                    return true;
+                }
+
+                //Check a process is attached.
+                if (hProcess == IntPtr.Zero)
+                {
+                    PrintHelp("The debugger must be attached to a process to use ERC");
+                    return true;
+                }
+
+                //Check the command was properly formed.
+                List<string> command = ParseCommand(argv[0]);
+                if (command == null)
+                {
+                    return true;
+                }
+                ERC.ErcCore core = new ERC.ErcCore();
+                ERC.ProcessInfo pi = new ERC.ProcessInfo(core, hProcess);
+
+                //This is the code for a popup box
+                /*
+                string Left = Interaction.InputBox("Enter value pls", "NetTest", "", -1, -1);
+                if (Left == null | Operators.CompareString(Left, "", false) == 0)
+                    PLog.WriteLine("[TEST] cancel pressed!");
+                else
+                    PLog.WriteLine("[TEST] line: {0}", Left);
+                */
+                PLog.WriteLine("Exiting plugin");
                 return true;
             }
-
-            if(hProcess == IntPtr.Zero)
+            catch(Exception e)
             {
-                PrintHelp("The debugger must be attached to a process to use ERC");
+                PLog.WriteLine(e.Message);
                 return true;
             }
-            ERC.ErcCore core = new ERC.ErcCore();
-            ERC.ProcessInfo pi = new ERC.ProcessInfo(core, hProcess);
-
-            //This is the code for a popup box
-            /*
-            string Left = Interaction.InputBox("Enter value pls", "NetTest", "", -1, -1);
-            if (Left == null | Operators.CompareString(Left, "", false) == 0)
-                PLog.WriteLine("[TEST] cancel pressed!");
-            else
-                PLog.WriteLine("[TEST] line: {0}", Left);
-            */
-            return true;
         }
 
         public static void PrintHelp(string errorMessage = null)
@@ -68,7 +70,7 @@ namespace ErcXdbg
             string help = "";
             help += "Global Options:\n";
             help += "   - \n";
-            help += "Usage: \n";
+            help += "Usage:       \n";
             help += "   --Config        |\n";
             help += "   --Pattern       |\n";
             help += "   --Bytearray     |\n";
@@ -88,8 +90,75 @@ namespace ErcXdbg
 
         public static List<string> ParseCommand(string command)
         {
+            List<string> parameters = new List<string>(command.Split(' '));
+            PLog.WriteLine("Parameters lenght = {0}", parameters.Count);
+            foreach(string s in parameters)
+            {
+                PLog.WriteLine(s);
+            }
+            parameters.RemoveAt(0);
+            PLog.WriteLine("Parameters lenght = {0}", parameters.Count);
+            foreach (string s in parameters)
+            {
+                PLog.WriteLine(s);
+            }
+            int commands = 0;
+            string option = "";
             
-            return null;
+            //check how many options were passed to ERC
+            foreach(string s in parameters)
+            {
+                if (s.Contains("--"))
+                {
+                    commands++;
+                    option = s.ToLower();
+                }
+            }
+
+            //Confirm the option is valid.
+            if(commands != 1)
+            {
+                PrintHelp("One option and it's parameters must be executed at a time (options start with --)");
+                return null;
+            }
+            else
+            {
+                switch (option)
+                {
+                    case "--config":
+                        break;
+                    case "--pattern":
+                        break;
+                    case "--bytearray":
+                        break;
+                    case "--compare":
+                        break;
+                    case "--assemble":
+                        break;
+                    case "--disassemble":
+                        break;
+                    case "--listprocesses":
+                        break;
+                    case "--processinfo":
+                        break;
+                    case "--moduleinfo":
+                        break;
+                    case "--threadinfo":
+                        break;
+                    case "--seh":
+                        break;
+                    case "--egghunters":
+                        break;
+                    case "--findnrp":
+                        break;
+                    case "--rop":
+                        break;
+                    default:
+                        PrintHelp("The command was not structured correctly: Option is not supported. ERC <option> <parameters>");
+                        return null;
+                }
+            }
+            return parameters;
         }
     }
 }
